558c6b668f68e8527ec94b7130a49b0d8cc8fa04
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-23 13:25:19 -0800

    fixed bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 $("#form").submit(function(event) {
-  var formInput = $("input#form");
+  var formInput = $("input#form-input").val();
   var toString = formInput.toString();
   var splitFormInput = formInput.split(" ");
-  alert(typeof splitFormInput);
   splitFormInput.forEach(function(element) {
-  var cloneArray = splitFormInput.slice(0);
-  var reverseArray = cloneArray.reverse(0);
+  var cloneArray = splitFormInput.slice();
+  var reverseArray = cloneArray.reverse();
+  alert(reverseArray);
+});
 });
 $("#button").submit(function(event) {
   alert("G")
   fromInput.push($("#from-input").val());
   $("#display").append("<h2>" + $("#from-input").val() + "</h2>");
   event.preventDefault();
-});
 });
 });
